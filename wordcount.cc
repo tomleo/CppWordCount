5b81cda4194ebf14f3d4091e788b85058389b5c6
Tom Leo <tomjleo@gmail.com>
2011-09-30 15:55:36 -0400

    Failed implementation 1

--- wordcount.cc
+++ wordcount.cc
@@ -8,6 +8,16 @@ WordCount::WordCount(){
     //Does nothing lolz
 }
 
+bool WordCount::find_word(string word){
+    dict::map_by<key>::const_iterator is = this->word_counts_.by<key>().find(word);
+    if (is != this->word_counts_.by<key>().end()){
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
 void WordCount::generate_words(string sentence){
 //    const char temp_array = sentence.c_str();
 //    int size = sizeof(sentence)/sizeof(*sentence);
@@ -39,29 +49,56 @@ void WordCount::generate_words(string sentence){
 //        cout << ++j << endl;
         cout << *i << endl;
     }*/
-    
 }
 
 void WordCount::get_wordcounts(){
-    DictIter di;
+    dict::map_by<key>::const_iterator is;
+    //= this->word_counts_.by<key>().find(word);
+    int end = this->words_.size();
+    for(int i=0; i<end; i++){
+        /*if(this->find_word(this->words_[i])){
+            this->word_counts_.push_back( keyVal(this->words_[i] ) );
+        }*/
+        is = this->word_counts_.by<key>().find(this->words_[i]);
+        if(is != this->word_counts_.by<key>().end()){
+//            this->word_counts_.push_back( keyVal(this->words_[i]));
+              //is->get<value>(this->words_[i])++;
+              //this->words_[i].left->value=10;
+              try {
+                this->word_counts_.left.at(this->words_[i]) = 100;
+              } catch (std::out_of_range & e){}
+        }
+        else {
+            this->word_counts_.push_back( keyVal(this->words_[i], 1));
+        }
+    }
+}
+
+/*void WordCount::get_wordcounts(){
+    //DictIter di;
     for(VecIter i = this->words_.begin(); i != words_.end(); ++i){
         for(VecIter j = i; j != words_.end(); ++j){
+            //If two words match
             if((*i).compare(*j) == 0){
+                //If word already in dictionary, add 1 to dict value
                 if(this->word_counts_.find(*i)!=di.end){
-                    word_counts_[*i]+=1;
+                    this->word_counts_.left.find(*i)
                 }
+                //Else add word as new key to dictionary
                 else {
-                    word_counts_[*i]=1;
+                    //word_counts_[*i]=1;
+                    word_counts_.push_back( keyVal( *i, 1));
                 }
             }
         }
     }
-}
+}*/
 
 int main(){
     string testString = "how, now now now brown cow cow";
     WordCount test;
     test.generate_words(testString);
+    cout << "FINISHED" << endl;
     return 0;
 }
 
--- wordcount.hh
+++ wordcount.hh
@@ -3,14 +3,31 @@
 #include <vector>
 #include <map>
 
-typedef std::map<std::string, int> Dict;
-typedef Dict::const_iterator DictIter;
+#include <boost/config.hpp>
+#include <boost/bimap/bimap.hpp>
+#include <boost/bimap/unordered_set_of.hpp>
+#include <boost/bimap/list_of.hpp>
+
+struct key {};
+struct value {};
+
+using namespace boost::bimaps;
+
+/*typedef boost::bimap<std::string, int> Dict;
+typedef Dict::value_type keyValue;*/
+typedef bimap<
+    unordered_set_of< tagged< std::string, key > >,
+    unordered_set_of< tagged< int, value > >,
+    list_of_relation
+> dict;
+typedef dict::value_type keyVal;
+
 typedef std::vector<std::string>::iterator VecIter;
 
 class WordCount{
 public:
     std::vector<std::string> words_;
-    Dict word_counts_;
+    dict word_counts_;
 
     WordCount();
 
@@ -21,6 +38,8 @@ public:
     // 2. compares all words against eachother
     // 3. creates a hash word:int count
     void get_wordcounts();
+
+    bool find_word(std::string);
 
 };
 
